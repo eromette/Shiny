5fa88e4f4d979dd7a9bb326c1a635da2f762970a
Cynism <axelfg@sfr.fr>
2020-05-12 16:06:29 +0200

    Update ui.R
    Co-Authored-By: eromette <eromette@users.noreply.github.com>

--- Shiny_Essai/ui.R
+++ Shiny_Essai/ui.R
@@ -5,23 +5,34 @@ library(shinythemes)
 #ligne : [X,]
 
 ui <- fluidPage(theme=shinytheme("sandstone"),
+                
   fluidRow(
-    tabPanel("Jeu",
-             tags$h1("Limite Limite Isariens",style="color:green", align="center")),
-    column(3,offset=1,
-                    sidebarPanel(width=80,
-                                 tags$h2("Jeu",style="color:blue",align="center"),
-                                 radioButtons("Chx",sample(dataLimP$Phrases,1),sample(dataLimM$Mots,4)),
-                                 actionButton("qsuiv","Question suivante",align="center"),
-                                 ),
-                    actionButton("register","Enregistrer la phrase",align="center")),
-    column(3,offset=4,
-                    sidebarPanel(width=200,
-                                 tags$h3("Phrases enregistrées",style="color:blue",align="center"),
-                                 )
-                    ),
-    tabPanel("Paramètres",
-             )
+    tabsetPanel(id="tabset",
+                
+                
+                tabPanel("Jeu",
+                         tags$h1("Limite Limite Isariens",style="color:green", align="center"),
+                         
+                         column(3,offset=1,
+                                sidebarPanel(width=80,
+                                             tags$h2("Jeu",style="color:blue",align="center"),
+                                             radioButtons("Chx",sample(dataLimP$Phrases,1),sample(dataLimM$Mots,4)),
+                                             actionButton("qsuiv","Question suivante",align="center"),
+                                             ),
+                                actionButton("register","Enregistrer la phrase",align="center")),
+                         
+                         column(3,offset=4,
+                                sidebarPanel(width=200,
+                                             tags$h3("Phrases enregistrées",style="color:blue",align="center"),
+                                             )
+                                )
+                         ),
+                
+                
+                tabPanel("Paramètres",
+                         tags$h1("Limite Limite Isariens",style="color:green", align="center")
+                         )
+                )
     )
   )
     
