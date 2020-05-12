647141dce5568ef92799ee185854563485b1aba5
Cynism <axelfg@sfr.fr>
2020-05-12 16:18:32 +0200

    Update ui.R
    Co-Authored-By: eromette <eromette@users.noreply.github.com>

--- Shiny_Essai/ui.R
+++ Shiny_Essai/ui.R
@@ -4,35 +4,41 @@ library(shinythemes)
 #colonne : [,X]
 #ligne : [X,]
 
-ui <- fluidPage(theme=shinytheme("sandstone"),
+ui <- fluidPage(theme=shinytheme("simplex"),
+    
+                tabsetPanel(id="tabset",
                 
-  fluidRow(
-    tabsetPanel(id="tabset",
-                
-                
-                tabPanel("Jeu",
-                         tags$h1("Limite Limite Isariens",style="color:green", align="center"),
+                            tabPanel("Jeu",
+                                     fluidRow(
+                                       tags$h1("Limite Limite Isariens",style="color:green", align="center"),
                          
-                         column(3,offset=1,
-                                sidebarPanel(width=80,
-                                             tags$h2("Jeu",style="color:blue",align="center"),
-                                             radioButtons("Chx",sample(dataLimP$Phrases,1),sample(dataLimM$Mots,4)),
-                                             actionButton("qsuiv","Question suivante",align="center"),
-                                             ),
-                                actionButton("register","Enregistrer la phrase",align="center")),
-                         
-                         column(3,offset=4,
-                                sidebarPanel(width=200,
-                                             tags$h3("Phrases enregistrées",style="color:blue",align="center"),
-                                             )
-                                )
-                         ),
-                
-                
-                tabPanel("Paramètres",
-                         tags$h1("Limite Limite Isariens",style="color:green", align="center")
-                         )
+                                       column(3,offset=1,
+                                              sidebarPanel(width=80,
+                                                           tags$h2("Jeu",style="color:blue",align="center"),
+                                                           radioButtons("Chx",sample(dataLimP$Phrases,1),sample(dataLimM$Mots,4)),
+                                                           actionButton("qsuiv","Question suivante",align="center"),
+                                                           )
+                                              ),
+                                       
+                                       column(3,offset=4,
+                                              sidebarPanel(width=200,
+                                             
+                                                           tags$h3("Phrases enregistrées",style="color:blue",align="center"),                                            )
+                                
+                                              ),
+                                       
+                                       
+                                     fluidRow(
+                                       column(12,offset=60,
+                                              actionButton("register","Enregistrer la phrase")
+                                              )
+                                       )
+                                     )
+                                     ),
+                            
+                            tabPanel("Paramètres",
+                                     tags$h1("Paramétrage du jeu",style="color:black", align="center")
+                                     )
+                            )
                 )
-    )
-  )
     
